2528706aa6f062345f8e7900b36a24649ee485fd
Riki Fridrich <riki@fczbkk.com>
2016-06-03 07:42:58 +0200

    rename file

---
+++ 10-basics/10-primitives.js
@@ -0,0 +1,18 @@
+// undefined
+console.log(typeof xxx);
+
+// null (object)
+console.log(typeof null);
+
+// boolean
+console.log(typeof false);
+console.log(typeof true);
+
+// number
+console.log(typeof 123.456);
+console.log(typeof Math.PI);
+
+
+// string
+console.log(typeof 'abc \'aaa\' bbb');
+console.log(typeof "def 'aaa' bbb");
