994c8e3866826207001ca024ab06c111665d1ea1
fczbkk <riki@fczbkk.com>
2019-03-20 03:55:09 +0100

    use Headless Chrome instead of PhantomJS

--- 50-testing/karma.conf.js
+++ 50-testing/karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function(config) {
       "coverage"
     ],
     browsers: [
-      "PhantomJS"
+      "ChromeHeadless"
     ],
     preprocessors: {
       "measure-pivot-distance.js": ["coverage"]
