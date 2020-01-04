59c2e01c544e7fc4b9d103e2edb1e782646a8d42
fczbkk <riki@fczbkk.com>
2020-01-04 09:57:04 +0100

    add primitive versus object example

---
+++ 20-concepts/15-reference/primitive-versus-object.js
@@ -0,0 +1,12 @@
+const name1 = 'Riki Fridrich'
+const name2 = 'Riki Fridrich'
+
+name1 === name2
+// true
+
+
+const person1 = {firstName: 'Riki', lastName: 'Fridrich'}
+const person2 = {firstName: 'Riki', lastName: 'Fridrich'}
+
+person1 === person2
+// false
